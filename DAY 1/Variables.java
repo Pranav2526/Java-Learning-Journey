5cb99baaf059a74f647ee120ae5e94427ce36782
Pranav Sonawane <pranavsonawane25@gmail.com>
2026-06-04 15:13:56 +0530

    Added Variables program

---
+++ DAY 1/Variables.java
@@ -0,0 +1,31 @@
+// A basic java code to demonstrate variables
+
+class Variables
+{
+    public static void main(String a[])
+    {
+        //num1 and num2 are variables of type 'int'
+        int num1 = 10;
+        int num2 = 10;
+        
+        //To print the values of num1 and num2
+        System.out.println(num1);
+        System.out.println(num2);
+
+        //To print the addition of num1 and num2
+        int result1 = num1 + num2;
+        System.out.println(result1);
+
+        //To print the subtraction of num1 and num2
+        int result2 = num1 - num2;
+        System.out.println(result2);
+
+        //To print the multiplication of num1 and num2
+        int result3 = num1 * num2;
+        System.out.println(result3);
+
+        //To print the division of num1 and num2
+        int result4 = num1 / num2;
+        System.out.println(result4);
+    }
+}
