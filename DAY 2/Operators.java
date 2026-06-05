62256f34ed768b1d28ff7f45428b06f625a4318d
Pranav Sonawane <pranavsonawane25@gmail.com>
2026-06-05 23:24:45 +0530

    Added Day 2: Operators

---
+++ DAY 2/Operators.java
@@ -0,0 +1,38 @@
+class Operators
+{
+    public static void main(String a[])
+    {
+        int num1 = 10;
+        int num2 = 20;
+        
+        // Addition operator
+        int result1 = num1 + num2;
+        System.out.println(result1);
+
+        // Subtraction operator
+        int result2 = num2 - num1;
+        System.out.println(result2);
+
+        // Multiplication operator
+        int result3 = num1 * num2;
+        System.out.println(result3);
+
+        // Division operator
+        int result4 = num1 / num2;
+        System.out.println(result4);
+
+        // Modulus operator
+        int result5 = num1 % num2;
+        System.out.println(result5);
+
+        // Pre-Increment/Decrement operator
+        int b = 5;
+        ++b;
+        System.out.println(b);  
+
+        // Post-Increment/Decrement operator
+        int c = 5;
+        c--;
+        System.out.println(c);  
+    }
+}
