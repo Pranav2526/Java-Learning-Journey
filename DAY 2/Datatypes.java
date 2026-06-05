145b95763cf0ce8be9e906b9e5740fc3eb106966
Pranav Sonawane <pranavsonawane25@gmail.com>
2026-06-05 23:24:19 +0530

    Added Day 2: Datatypes

---
+++ DAY 2/Datatypes.java
@@ -0,0 +1,22 @@
+class Datatypes
+{
+    public static void main(String a[])
+    {
+        // Integers -- Byte, Short, Long and Int
+        int i = 10;       // bytes = 4
+        long l = 12345678l;      // bytes = 8  (when using long always add L at the end of the number)
+        short s = 100;    // bytes = 2
+        byte b = 20;      // bytes = 1 
+        
+        // Float -- double and float
+        float f = 5.8f;   // bytes = 4 (when using float always add F at the end of the number)
+        double d = 5.8;   // bytes = 8
+        
+        // Character -- char
+        char ch = 'A';   // bytes = 2  (always use single quotes for char)
+        
+        // Boolean -- boolean (true or false)
+        boolean bo = true;   // bytes = 1 (can only take true or false)
+    }
+
+}
