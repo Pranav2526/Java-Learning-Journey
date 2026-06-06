a904263c2b60b2070e68b7b6ca7e205064abfa62
Pranav Sonawane <pranavsonawane25@gmail.com>
2026-06-06 22:51:48 +0530

    Switch Statement Basic Program

---
+++ DAY 3/SwitchStatement.java
@@ -0,0 +1,34 @@
+class SwitchCase
+{
+    public static void main(String a[])
+    {
+        int num = 4;
+
+        switch(num)
+        {
+            case 1:
+                System.out.println("MONDAY");
+                break;
+            case 2:
+                System.out.println("TUESDAY");
+                break;
+            case 3:
+                System.out.println("WEDNESDAY");
+                break;
+            case 4:
+                System.out.println("THURSDAY");
+                break;
+            case 5:
+                System.out.println("FRIDAY");
+                break;
+            case 6:
+                System.out.println("SATURDAY");
+                break;
+            case 7:
+                System.out.println("SUNDAY");
+                break;
+            default:
+                System.out.println("Enter a valid number");
+        }
+    }
+}
