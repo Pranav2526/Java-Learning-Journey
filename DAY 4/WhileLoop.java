38ff30ea5d9837cc7fc1cb3cc7113edd02d9f429
Pranav Sonawane <pranavsonawane25@gmail.com>
2026-06-07 22:25:31 +0530

    While Loop

---
+++ DAY 4/WhileLoop.java
@@ -0,0 +1,24 @@
+class WhileLoop
+{
+    public static void main(String a[])
+    {
+        // While Loop
+        int i = 1;
+
+        while(i<=10)
+        {
+            System.out.println(i + " Hi, My name is Pranav");
+            i++;
+        }
+
+
+        // Do-While Loop
+        int j = 5;
+
+        do
+        {
+            System.out.println("Hi, My name is Pranav");
+            j++;
+        } while(j<=4);
+    }
+}
