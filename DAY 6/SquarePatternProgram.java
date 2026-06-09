16af620f42e048c7d6ee411bf5641fb9a27e6f72
Pranav Sonawane <pranavsonawane25@gmail.com>
2026-06-09 16:57:53 +0530

    Square star pattern program

---
+++ DAY 6/SquarePatternProgram.java
@@ -0,0 +1,11 @@
+public class SquarePatternProgram
+{
+    public static void main(String args[])
+    {
+        // Using For loop
+        for(int line = 1; line<=3; line++)
+            {
+                System.out.println("*****");
+            } 
+    }
+}
