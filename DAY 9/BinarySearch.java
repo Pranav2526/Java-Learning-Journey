d9c8402e0e2c34ff45f899e5bfd67f4b27404584
Pranav Sonawane <pranavsonawane25@gmail.com>
2026-06-12 14:57:21 +0530

    Added Day 9: Binary Search logic

---
+++ DAY 9/BinarySearch.java
@@ -0,0 +1,37 @@
+public class BinarySearch
+{
+    public static void main(String args[])
+    {
+        int numbers[] = {2, 4, 6, 8, 10, 12, 14};
+        int key = 10;
+        System.out.println("Index for key is: " + binarysearch(numbers, key));
+    }
+
+    public static int binarysearch(int numbers[], int key)
+    {
+        int start = 0, end = numbers.length - 1;
+
+        while(start<=end)
+        {
+            int mid = (start+end) / 2;
+
+            // Comparisons
+            //Found
+            if(numbers[mid] == key)
+            {
+                return mid;
+            }
+            // Left
+            if(numbers[mid] > key)
+            {
+                end = mid - 1;
+            }
+            // Right
+            else
+            {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
