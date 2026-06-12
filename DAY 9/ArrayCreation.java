a0ca7102389e27bea1ec37d7296b9e8e44c47152
Pranav Sonawane <pranavsonawane25@gmail.com>
2026-06-12 14:54:44 +0530

    Array creation

---
+++ DAY 9/ArrayCreation.java
@@ -0,0 +1,16 @@
+// Creating Array
+
+public class ArrayCreation
+{
+    public static void main(String args[])
+    {
+        // Example 1
+        int marks[] = new int[5];
+
+        // Example 2
+        int numbers[] = {1, 2, 3};
+
+        // Example 3
+        String fruits[] = {"apple", "banana", "mango"};
+    }
+}
