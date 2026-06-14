6e3b43e3493d9d15cf6aa8d22610a2a425966940
Pranav Sonawane <pranavsonawane25@gmail.com>
2026-06-14 16:32:10 +0530

    Insertion Sort algorithm logic

---
+++ DAY 11/InsertionSort.java
@@ -0,0 +1,36 @@
+public class InsertionSort
+{
+    public static void main(String args[])
+    {
+        int arr[] = {5, 4, 1, 3, 2};
+        sort(arr);
+        PrintArray(arr);
+    }
+
+    public static void sort(int arr[])
+    {
+        for(int i = 1; i<arr.length; i++)
+        {
+            int curr = arr[i];
+            int prev = i-1;
+
+            // Finding out the correct position to insert
+            while(prev >= 0 && arr[prev] > curr)
+            {
+                arr[prev+1] = arr[prev];
+                prev--;
+            }
+            // Insertion
+            arr[prev+1] = curr;
+        }
+    }
+
+    public static void PrintArray(int arr[])
+    {
+        for(int i = 0; i<arr.length; i++)
+        {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
